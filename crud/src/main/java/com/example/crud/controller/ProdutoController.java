22eca3ff65d59c7ca73d058bb61cb4097689c2dc
Vinicius <sg891847@gmail.com>
2026-04-27 00:24:51 -0300

    Add files via upload

---
+++ crud/src/main/java/com/example/crud/controller/ProdutoController.java
@@ -0,0 +1,52 @@
+package com.example.crud.controller;
+
+import com.example.crud.model.Produto;
+import com.example.crud.repository.ProdutoRepository;
+import com.example.crud.service.ProdutoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoRepository produtoRepository, ProdutoService produtoService) {
+        this.produtoRepository = produtoRepository;
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping("/formulario")
+    public String exibirFormulario(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "formulario";
+    }
+    @PostMapping("/salvar")
+    public String salvarProduto(@ModelAttribute Produto produto) {
+        produtoService.salvar(produto);
+        return "redirect:/produto/listar";
+    }
+    @GetMapping("/listar")
+    public String litarProdutos(Model model) {
+        List<Produto> produtos = produtoRepository.findAll();
+        model.addAttribute("produtos", produtos);
+        return "lista";
+    }
+    @GetMapping("/deletar/{id}")
+    public String deletarProduto(@PathVariable Integer id) {
+        produtoRepository.deleteById(id);
+        return "redirect:/produto/listar";
+    }
+    @GetMapping("/editar/{id}")
+    public String editarProduto(@PathVariable Integer id, Model model) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        model.addAttribute("produto", produto);
+        return "formulario";
+    }
+}
