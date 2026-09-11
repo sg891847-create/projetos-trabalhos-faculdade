2fbafaf7af2d8681227e6e31ba95ac24d548df23
Vinicius <sg891847@gmail.com>
2026-09-11 10:22:26 -0300

    Add files via upload

---
+++ sistemas distribuidos/AtividadeistemasDistribuidos/src/atividadeistemasdistribuidos/Pessoa.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividadeistemasdistribuidos;
+
+/**
+ *
+ * @author Windows 11
+ */
+public class Pessoa {
+    
+}
